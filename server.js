95e4c0a43981e5c9b6969c6d911bd510ed58161e
vitalspace <myglobalsounds@gmail.com>
2022-07-12 18:56:51 +0000

    Update index.html and server.js

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 import { serve, file } from "bun";
-// Create Server
+// Create Server.
 serve({
   fetch(request) {
-    // Get Url and method from request
+    // Get Url and method from request.
     const { url, method } = request;
-    // Get Pathname form url
+    // Get Pathname form url.
     const { pathname } = new URL(url);
-    // Get archive index html with file method
+    // Get archive index html with file method.
     const html = file("index.html");
     if (pathname === "/" && method === "GET") {
       return new Response(html, {
